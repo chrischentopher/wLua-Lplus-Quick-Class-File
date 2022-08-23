987dcdc1c3b761661008b448bc2ae70888035798
chrischentopher <46310072+chrischentopher@users.noreply.github.com>
2022-08-23 17:04:13 +0800

    support change author name

--- LplusFactory/LplusFactoryInit.lua
+++ LplusFactory/LplusFactoryInit.lua
@@ -1,13 +1,15 @@
-_G.LUA_PATH =  'G:\\svn\\phecda_client\\trunk\\Azure\\Output\\Lua'
+_G.LF_LUA_PATH =  'G:\\svn\\phecda_client\\trunk\\Azure\\Output\\Lua'
+_G.LF_AUTHOR_NAME = 'chentao'
 _G.LplusFactoryIsOuter = true --是否外部版，外部版反在pck下或private\RunGamePck\More下
 if not _G.LplusFactoryData then
 	if _G.LplusFactoryIsOuter then --外部版不能用reload
-		_G.CT_FILE_PATH = 'G:\\svn\\phecda_client\\trunk\\private\\RunGamePck\\More\\LplusFactory'
+		_G.LF_FILE_PATH = 'G:\\svn\\phecda_client\\trunk\\private\\RunGamePck\\More\\LplusFactory'
 		_G.LplusFactoryData = dofile"More/LplusFactory/LplusFactoryData.lua"
 		_G.LplusFactory = dofile"More/LplusFactory/LplusFactory.lua".Instance()
 	else
-		_G.CT_FILE_PATH = _G.LUA_PATH..'\\LplusFactory'
+		_G.LF_FILE_PATH = _G.LUA_PATH..'\\LplusFactory'
 		_G.LplusFactoryData = require "LplusFactory.LplusFactoryData"
 		_G.LplusFactory = require "LplusFactory.LplusFactory".Instance()
 	end
 end
+print('module More.LplusFactory init success')
